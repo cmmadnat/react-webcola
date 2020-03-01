046e9141fba3ef92b2dbfad01984cb6d922dda81
cmmadnat <cmmadnat@gmail.com>
2020-03-01 17:26:13 +0700

    add all other file

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Component } from 'react'
+
+export default class extends Component {
+  render() {
+    return <div>
+      <h2>Welcome to React components</h2>
+    </div>
+  }
+}
